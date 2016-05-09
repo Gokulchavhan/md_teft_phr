9c78567c5bbcf8759749996edb6931fb446954c7
Christian.heyd <christian.heyd@feisystems.com>
2016-05-09 15:38:04 -0400

    Added data fields for Client Information section

--- application/views/pages/userprofile.php
+++ application/views/pages/userprofile.php
@@ -13,7 +13,7 @@
           <div class="panel-heading">
             <h4 class="panel-title">
               <a data-toggle="collapse" href="#collapseOne">
-                User Info
+                General Information
               </a>
             </h4>
           </div>
@@ -22,14 +22,14 @@
               <div class="row">
                 <div class="col-sm-offset-1 col-sm-4">
                     <address>
-                      <strong>Name:</strong><br>
-                      Cicio A Smith
+                      <strong>First Name:</strong><br>
+                      Cicio
                     </address>
                 </div>	
                 <div class="col-sm-4">
                     <address>
-                      <strong>Suffix:</strong><br>
-                     Sr.
+                      <strong>Middle Name:</strong><br>
+                     A.
                     </address>
                  </div>	
                  <div class="col-xs-3 text-right">
@@ -39,8 +39,23 @@
              <div class="row">
                 <div class="col-sm-offset-1 col-sm-4">
                     <address>
+                      <strong>Last Name:</strong><br>
+                      Smith
+                    </address>
+                </div>	
+                <div class="col-sm-7">
+                    <address>
+                      <strong>Suffix:</strong><br>
+                      Male
+                    </address>
+                 </div>
+            </div>
+            
+             <div class="row">
+                <div class="col-sm-offset-1 col-sm-4">
+                    <address>
                       <strong>Date of Birth:</strong><br>
-                      01/01/1950
+                      08/12/1991
                     </address>
                 </div>	
                 <div class="col-sm-7">
@@ -55,24 +70,23 @@
                 <div class="col-sm-offset-1 col-sm-4">
                     <address>
                       <strong>Race:</strong><br>
-                      Caucasion (Not Hispanic)
+                      Caucasian
                     </address>
-                </div>	
+                </div>
                 <div class="col-sm-7">
                     <address>
-                      <strong>Marital Status:</strong><br>
-                      Married
+                        <strong>Hispanic:</strong><br>
+                        No
                     </address>
-                 </div>	
+                </div>
             </div>
-            
-             <div class="row">
+            <div class="row">
                 <div class="col-sm-offset-1 col-sm-4">
                     <address>
-                      <strong>Languages:</strong><br>
-                      English, French
+                        <strong>Jurisdiction/County:</strong><br>
+                        Montgomery
                     </address>
-                </div>	
+                </div>
             </div>
             </div>
           </div>
@@ -81,7 +95,7 @@
           <div class="panel-heading">
             <h4 class="panel-title">
               <a data-toggle="collapse" href="#collapseTwo">
-                Contact Info
+                Additional Client Information
               </a>
             </h4>
           </div>
@@ -90,15 +104,14 @@
 				 <div class="row">
                 <div class="col-sm-offset-1 col-sm-4">
                     <address>
-                      <strong>Address:</strong><br>
-                      1 Main Street<br>
-                      Ashton, MS 38603
+                      <strong>SSN:</strong><br>
+                      ***-**-1234
                     </address>
                 </div>	
                 <div class="col-sm-4">
                     <address>
-                      <strong>County:</strong><br>
-                     Benton
+                      <strong>Client ID:</strong><br>
+                     1234567890
                     </address>
                  </div>	
                  <div class="col-xs-3 text-right">
@@ -108,48 +121,201 @@
              <div class="row">
                 <div class="col-sm-offset-1 col-sm-4">
                 <address>
-                      <strong>Phone:</strong><br>
-                      Mobile: (436)-653-3645<br>
-                      Work: (436)-374-8234 Ext: 6841<br>
+                      <strong>Marital Status:</strong><br>
+                      Married
                     </address>
                 </div>	
                 <div class="col-sm-7">
                     <address>
-                      <strong>Email:</strong><br>
-                      casmith@example.com
+                      <strong>Primary Language:</strong><br>
+                      English
                     </address>
                  </div>	
             </div>
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-4">
+                    <address>
+                        <strong>Client Address:</strong><br>
+                        123 Main St.,<br>
+                        Silver Spring, MD 20901
+                    </address>
+                </div>
+                <div class="col-sm-7">
+                    <address>
+                        <strong>Legal Guardian Full Name:</strong><br>
+                        John Doe
+                    </address>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-4">
+                    <address>
+                        <strong>Guardian Relationship with Client:</strong><br>
+                        Sibling
+                    </address>
+                </div>
+                <div class="col-sm-7">
+                    <address>
+                        <strong>Client Phone #:</strong><br>
+                        (240) 305-2412
+                    </address>
+                </div>
+            </div>
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-4">
+                        <address>
+                            <strong>Guardian Phone #:</strong><br>
+                            (240) 305-2412
+                        </address>
+                    </div>
+                    <div class="col-sm-7">
+                        <address>
+                            <strong>Client Alternate Phone #:</strong><br>
+                            N/A
+                        </address>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-4">
+                        <address>
+                            <strong>Guardian Alternate Phone #:</strong><br>
+                            (301) 555-1234
+                        </address>
+                    </div>
+                    <div class="col-sm-7">
+                        <address>
+                            <strong>OTP Assignment:</strong><br>
+                            Yes
+                        </address>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-4">
+                        <address>
+                            <strong>OTP Assignment History:</strong><br>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <td><strong>OTP Serial #</strong></td>
+                                        <td><strong>Assignment Date</strong></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>123456789012345</td>
+                                        <td>08/12/1991</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </address>
+                    </div>
+                    <div class="col-sm-7">
+                        <address>
+                            <strong>Client OTP Serial #:</strong><br>
+                            123456789012345
+                        </address>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-4">
+                        <address>
+                            <strong>Program Enrollment:</strong><br>
+                            AL
+                        </address>
+                    </div>
+                    <div class="col-sm-7">
+                        <address>
+                            <strong>Supports Planner:</strong><br>
+                            Jane Doe
+                        </address>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-4">
+                        <address>
+                            <strong>Support Planning Agency:</strong><br>
+                            Agency Name, Inc.
+                        </address>
+                    </div>
+                </div>
             </div>
           </div>
         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" href="#collapseThree">
+                        Client Medicaid Information
+                    </a>
+                </h4>
+            </div>
+             <div id="collapseThree" class="panel-collapse collapse in">
+                 <div class="panel-body">
+                     <div class="row">
+                         <div class="col-sm-offset-1 col-sm-4">
+                             <address>
+                                 <strong>MA#:</strong><br>
+                                 44444444444 (Community)
+                             </address>
+                         </div>
+                         <div class="col-sm-7">
+                             <address>
+                                 <strong>Coverage Group:</strong>
+                                 Individuals Eligible for Home and Community-Based Services
+                             </address>
+                         </div>
+                     </div>
+                     <div class="row">
+                         <div class="col-sm-offset-1 col-sm-4">
+                             <address>
+                                 <strong>Start Date:</strong>
+                                 08/12/1991
+                             </address>
+                         </div>
+                         <div class="col-sm-7">
+                             <address>
+                                 <strong>End Date:</strong>
+                                 08/12/1991
+                             </address>
+                         </div>
+                     </div>
+                     <div class="row">
+                         <div class="col-sm-offset-1 col-sm-4">
+                             <address>
+                                 <strong>LTC/Community:</strong>
+                                 Community
+                             </address>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
 		<div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
-              <a data-toggle="collapse" href="#collapseThree">
-                Account Info
+              <a data-toggle="collapse" href="#collapseFour">
+                Client Login Information
               </a>
             </h4>
           </div>
-          <div id="collapseThree" class="panel-collapse collapse in">
+          <div id="collapseFour" class="panel-collapse collapse in">
             <div class="panel-body">
-              <form class="form-horizontal" role="form">
-				  <div class="form-group">
-					<label for="username" class="col-xs-3 control-label">User Name</label>
-					<div class="col-xs-6">
-					  <p class="form-control-static">CSmith</p>
-					</div>
-					<div class="col-xs-3 text-right">
-					  <a class="btn btn-sm btn-default" href="#" role="button" data-toggle="modal" data-target="#editaccountinfo">Edit</a>
-					</div>
-				  </div>
-				  <div class="form-group">
-					<label for="password" class="col-xs-3 control-label">Password</label>
-					<div class="col-xs-6">
-					  <p class="form-control-static">**********</p>
-					</div>
-				  </div>
-			  </form>	
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-4">
+                        <address><strong>User Name:</strong><br>CSmith</address>
+                    </div>
+                    <div class="col-sm-7">
+                        <address><strong>Email Address:</strong><br>casmith@live.com</address>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-4">
+                        <address>
+                            <strong>Alternate Email Address:</strong><br>
+                            casmith2@live.com
+                        </address>
+                    </div>
+                </div>
             </div>
           </div>
         </div>
