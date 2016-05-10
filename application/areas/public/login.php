b07ed689c443e2a874b2b1df08f39070811d17a2
Luis Najera <lutech1@mac.com>
2016-05-10 13:49:02 -0400

    Made enhancements to the Input Form Fields and Login page.

--- application/areas/public/login.php
+++ application/areas/public/login.php
@@ -1,8 +1,19 @@
+<style>
+    .login-icon {
+        position: absolute;
+        margin-left: -35px;
+        margin-top: 12px;
+        font-size: 130%;
+        text-align: center;
+        width: 30px;
+        color: #999;
+    }
+</style>
 
 <form class="form-signin" id="loginform" method="post" name="form" action="<?php echo base_url().'index.php/site/attemptLogin'?>">
-    <h2 class="form-signin-heading" id="myModalLabel">User Login</h2>
-    <input type="text" class="form-control" placeholder="User Name" name="username" required autofocus title="roisered (Public Member 1); gabrielgreen (Public Member 2); penelopepurple (Un-authorized Provider); susanasilver (Authorized Provider); burtonbrown (DOM Staff)">
-    <input type="password" class="form-control" placeholder="Password"  name="usernamepassword" required title="Type anything for password">
+    <h2 class="form-signin-heading" id="myModalLabel" style="color: #428BCA"><b>MyCare</b>&nbsp;Login</h2>
+    <span class="fa fa-user login-icon"></span><input type="text" class="form-control" placeholder="User Name" name="username" required autofocus title="roisered (Public Member 1); gabrielgreen (Public Member 2); penelopepurple (Un-authorized Provider); susanasilver (Authorized Provider); burtonbrown (DOM Staff)">
+    <span class="fa fa-lock login-icon"></span><input type="password" class="form-control" placeholder="Password"  name="usernamepassword" required title="Type anything for password">
 <!--    <label class="checkbox">-->
 <!--        <input type="checkbox" value="remember-me"> Remember me-->
 <!--    </label>-->
@@ -11,3 +22,4 @@
     <br>
     <a href="#">Forgot User Name?</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">Forgot Password?</a>
 </form>
+
