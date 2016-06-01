e3447368269e74eca8185ceb0eff62a4b15a7ed2
luis.najera <lutech1@mac.com>
2016-06-01 16:38:26 -0400

    Fixed minor issue with dashboard.

--- application/areas/home/home.php
+++ application/areas/home/home.php
@@ -4,6 +4,28 @@
 <!--    </div>-->
 <!--</div>-->
 <div class="col-md-12">
+    <row>
+        <div class="col-sm-12 no-padding">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="col-xs-6">
+                        <h4>
+                            <span class="secondary-color">Plan of Service Summary</span><br>
+                            <span class="h5 no-margin"><small><i>as of 03/15/2016</i></small></span>
+                        </h4>
+                    </div>
+                        <div class="col-xs-6">
+                            <div class="col-sm-6">
+                                <span class="value h4"><small>effective date</small><br><span>03/15/2016</span></span>
+                            </div>
+                            <div class="col-sm-6">
+                                <span class="value h4"><small>end date</small><br><span>03/14/2017</span></span>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </row>
     <div class="row">
         <div class="col-lg-12">
             <?php require "shared/_ServiceActivityTracker_v2.php" ?>
